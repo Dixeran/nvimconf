f42fbbc0bbefe6ce7aeaf3743293260cd8be5fbd
yinjianian.0 <yinjianian.0@bytedance.com>
2023-06-02 13:57:58 +0800

    init

---
+++ init.lua
@@ -0,0 +1,110 @@
+print("hello world")
+
+vim.cmd [[packadd packer.nvim]]
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+
+require('neoscroll').setup()
+require("bufferline").setup{}
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  hijack_cursor = true,
+  view = {
+    width = 30,
+    adaptive_size = true,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "ruby", "python" },
+  highlight = {
+    enable = true,
+  },
+  auto_install = true,
+}
+
+-- set colro theme
+vim.cmd [[colorscheme tokyonight-moon]]
+
+-- set nvim-tree autoclose
+vim.api.nvim_create_autocmd({"QuitPre"}, {
+    callback = function() vim.cmd("NvimTreeClose") end,
+})
+
+require('coc_keymap')
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end
+  }
+  use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
+  use 'folke/tokyonight.nvim'
+  use {'neoclide/coc.nvim', branch = 'release'}
+  use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
+  use 'karb94/neoscroll.nvim'
+end)
